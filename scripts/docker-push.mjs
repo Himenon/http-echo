506ac2f2649217f7f4735ac1b4a348d9de6a7348
K.Himeno <k.himeno314@gmail.com>
2022-05-16 20:08:25 +0900

    chore: add latest (#6)

--- scripts/docker-push.mjs
+++ scripts/docker-push.mjs
@@ -5,3 +5,4 @@ const pkg = JSON.parse(content.toString());
 
 const actor = process.env.DOCKER_USER_NAME.toLowerCase();
 await $`docker push ghcr.io/${actor}/http-echo:${pkg.version}`;
+await $`docker push ghcr.io/${actor}/http-echo:latest`;
