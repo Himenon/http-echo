6411e39be627e617bb119231ae93640ea018566b
Himenon <k.himeno314@gmail.com>
2022-05-16 14:56:21 +0900

    chore: update

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,26 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
 import * as os from "os";
 import concat from "concat-stream";
 import jwt from "jsonwebtoken";
 
-const app = express()
+const app = express();
 
-app.set('json spaces', 2);
+app.set("json spaces", 2);
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.use(function(req, res, next){
-  req.pipe(concat(function(data){
-    req.body = data.toString('utf8');
-    next();
-  }));
+app.use(function (req, res, next) {
+  req.pipe(
+    concat(function (data) {
+      req.body = data.toString("utf8");
+      next();
+    }),
+  );
 });
 
-app.all('*', (req, res) => {
-  const echo = {
+app.all("*", (req, res) => {
+  const echo: Record<string, unknown> = {
     path: req.path,
     headers: req.headers,
     method: req.method,
@@ -33,15 +35,15 @@ app.all('*', (req, res) => {
     subdomains: req.subdomains,
     xhr: req.xhr,
     os: {
-      hostname: os.hostname()
-    }
+      hostname: os.hostname(),
+    },
   };
   if (process.env.JWT_HEADER) {
     const token = req.headers[process.env.JWT_HEADER.toLowerCase()];
     if (!token) {
       echo.jwt = token;
-    } else {
-      const decoded = jwt.decode(token, {complete: true});
+    } else if (typeof token === "string") {
+      const decoded = jwt.decode(token, { complete: true });
       echo.jwt = decoded;
     }
   }
@@ -51,19 +53,19 @@ app.all('*', (req, res) => {
 const server = app.listen(process.env.PORT || 80);
 let calledClose = false;
 
-process.on('exit', function () {
+process.on("exit", function () {
   if (calledClose) return;
-  console.log('Got exit event. Trying to stop Express server.');
-  server.close(function() {
+  console.log("Got exit event. Trying to stop Express server.");
+  server.close(function () {
     console.log("Express server closed");
   });
 });
 
-process.on('SIGINT', function() {
-  console.log('Got SIGINT. Trying to exit gracefully.');
+process.on("SIGINT", function () {
+  console.log("Got SIGINT. Trying to exit gracefully.");
   calledClose = true;
-  server.close(function() {
+  server.close(function () {
     console.log("Exoress server closed. Asking process to exit.");
-    process.exit()
+    process.exit();
   });
 });
