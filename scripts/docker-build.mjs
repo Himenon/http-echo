5604b926a2d4a06977cc3e1f3e649655208261ea
Himenon <k.himeno314@gmail.com>
2022-05-16 19:51:04 +0900

    chore: remove eslint

--- scripts/docker-build.mjs
+++ scripts/docker-build.mjs
@@ -3,4 +3,5 @@
 const content = await fs.readFile("./package.json");
 const pkg = JSON.parse(content.toString());
 
-await $`docker build . -t ghcr.io/himenon/http-echo:${pkg.version} --no-cache`;
+const actor = process.env.GITHUB_ACTOR.toLowerCase();
+await $`docker build . -t ghcr.io/${actor}/http-echo:${pkg.version} --no-cache`;
